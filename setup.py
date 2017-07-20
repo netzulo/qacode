7cd203c5224487ed599bea52852a99717775783c
netzulo <netzuleando@gmail.com>
2017-07-20 02:01:07 +0200

    [qacode] v0.0.0
    + Added integration for Windows with appveyor
    + Added support for pypi package

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
 
-setup(name='qalabCore',
+setup(name='qacode',
       version='0.0.0',
-      packages=['qalabCore'],
+      packages=['qacode'],
+      description = 'Main automation lib',
+      author = 'Netzulo Open Source',
+      author_email = 'netzuleando@gmail.com',
+      url = 'https://github.com/netzulo/qacode', # use the URL to the github repo
+      download_url = 'https://github.com/netzulo/qacode/tarball/0.0.0',
+      keywords = ['testing', 'logging', 'functional','selenium', 'test'],
       entry_points={ 'console_scripts': [] },
       )
