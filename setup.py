5d8c3f9fa345c7dc017b80be1432ec6a60f7eed2
netzulo <netzuleando@gmail.com>
2017-07-20 12:16:51 +0200

    [qacode] updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+
 from setuptools import setup
 
 
-setup(name='qalabCore',
-      version='0.0.0',
-      packages=['qalabCore'],
-      entry_points={ 'console_scripts': [] },
+setup(name='qacode',
+      version='0.0.1',
+      packages=['qacode'],
+      description = 'Main automation lib',
+      author = 'Netzulo Open Source',
+      author_email = 'netzuleando@gmail.com',
+      url = 'https://github.com/netzulo/qacode', # use the URL to the github repo
+      download_url = 'https://github.com/netzulo/qacode/tarball/0.0.1',
+      keywords = ['testing', 'logging', 'functional','selenium', 'test'],
       )
