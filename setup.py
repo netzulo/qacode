197717477299be8051e29ddf5bad2acbc7f1f7ec
netzulo <netzuleando@gmail.com>
2017-07-20 02:24:46 +0200

    [qacode] UPLOAD project to pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup
 
 setup(name='qacode',
       version='0.0.0',
-      packages=['qacode'],
+      packages=['core', 'tests', 'configs', 'logs'],
       description = 'Main automation lib',
       author = 'Netzulo Open Source',
       author_email = 'netzuleando@gmail.com',
       url = 'https://github.com/netzulo/qacode', # use the URL to the github repo
       download_url = 'https://github.com/netzulo/qacode/tarball/0.0.0',
       keywords = ['testing', 'logging', 'functional','selenium', 'test'],
-      entry_points={ 'console_scripts': [] },
       )
