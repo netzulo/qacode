b790dfdf4ed195522a42ced0867707285dfb43e6
netzulo <netzuleando@gmail.com>
2017-07-21 21:35:35 +0200

    [qacode] FIX for travis python 3.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='qacode',
       download_url = 'https://github.com/netzulo/qacode/tarball/v0.0.5',
       keywords = ['testing', 'logging', 'functional','selenium', 'test'],
       install_requires=[
-          'appdirs==1.4.2','packaging==16.8','pyparsing',
+          'appdirs','packaging==16.8','pyparsing',
           'selenium==3.0.2','six==1.10.0','nose==1.3.7','nose-testconfig==0.10',
           'TestLink-API-Python-client==0.6.2']
       )
