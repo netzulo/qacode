1d0ce5949e4ad950b4e28cebb0e9465d737ff920
netzulo <netzuleando@gmail.com>
2017-07-21 21:09:12 +0200

    [qacode] UPDATE to v0.0.3

--- setup.py
+++ setup.py
@@ -3,24 +3,18 @@ from os import path
 from os import name
 
 CURR_PATH = path.abspath(path.dirname(__file__))
-FORMAT_PATH = "{}{}{}"
-
-if name == "nt":
-    requirements_path = FORMAT_PATH.format(CURR_PATH,"\\","requirements.txt")
-else:
-    requirements_path = FORMAT_PATH.format(CURR_PATH,"/","requirements.txt")
-
-with open(requirements_path) as f:
-    requirements = f.read().splitlines()
 
 setup(name='qacode',
-      version='0.0.2',
+      version='0.0.3',
       packages=['qacode'],
       description = 'Main automation lib',
       author = 'Netzulo Open Source',
       author_email = 'netzuleando@gmail.com',
-      url = 'https://github.com/netzulo/qacode', # use the URL to the github repo
-      download_url = 'https://github.com/netzulo/qacode/tarball/v0.0.2',
+      url = 'https://github.com/netzulo/qacode',
+      download_url = 'https://github.com/netzulo/qacode/tarball/v0.0.3',
       keywords = ['testing', 'logging', 'functional','selenium', 'test'],
-      install_requires=requirements,
+      install_requires=[
+          'appdirs==1.4.2','packaging==16.8','pyparsing==2.1.10',
+          'selenium==3.0.2','six==1.10.0','nose==1.3.7','nose-testconfig==0.10',
+          'TestLink-API-Python-client==0.6.2']
       )
