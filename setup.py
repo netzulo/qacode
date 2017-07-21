b2074bb9c49a10963a8d8aa3852b216f6b153ab3
netzulo <netzuleando@gmail.com>
2017-07-21 21:30:17 +0200

    [qacode] UPDATE to v0.0.4

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from os import path
 from os import name
 
@@ -6,7 +6,7 @@ CURR_PATH = path.abspath(path.dirname(__file__))
 
 setup(name='qacode',
       version='0.0.3',
-      packages=['qacode'],
+      packages=find_packages(exclude=['tests']),
       description = 'Main automation lib',
       author = 'Netzulo Open Source',
       author_email = 'netzuleando@gmail.com',
@@ -14,7 +14,7 @@ setup(name='qacode',
       download_url = 'https://github.com/netzulo/qacode/tarball/v0.0.3',
       keywords = ['testing', 'logging', 'functional','selenium', 'test'],
       install_requires=[
-          'appdirs==1.4.2','packaging==16.8','pyparsing==2.1.10',
+          'appdirs==1.4.2','packaging==16.8','pyparsing',
           'selenium==3.0.2','six==1.10.0','nose==1.3.7','nose-testconfig==0.10',
           'TestLink-API-Python-client==0.6.2']
       )
