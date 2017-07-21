a31314467ce214e4eaccf4fb12197451dfe1daba
netzulo <netzuleando@gmail.com>
2017-07-21 18:35:37 +0200

    [qacode] UPDATE to v0.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='qacode',
       author = 'Netzulo Open Source',
       author_email = 'netzuleando@gmail.com',
       url = 'https://github.com/netzulo/qacode', # use the URL to the github repo
-      download_url = 'https://github.com/netzulo/qacode/tarball/0.0.1',
+      download_url = 'https://github.com/netzulo/qacode/tarball/v0.0.1',
       keywords = ['testing', 'logging', 'functional','selenium', 'test'],
       install_requires=requirements,
       )
