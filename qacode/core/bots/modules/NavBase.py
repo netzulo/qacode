07a8d085976611c8106519afcde1156ed61f3ff9
ntz <netzuleando@gmail.com>
2017-07-29 17:13:42 +0200

    [qacode] sonarqube taks... resolving issues

--- qacode/core/bots/modules/NavBase.py
+++ qacode/core/bots/modules/NavBase.py
@@ -4,182 +4,226 @@ Created on 04 march 2017
 @author: ntz
 '''
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from qacode.core.exceptions.CoreException import CoreException
 
 class NavBase(object):
     '''
-    classdocs
+    Main navigation methods to use on selenium scripts
+    + bot
+    + driver
     '''
+    bot = None
+    driver = None
 
     def __init__(self, bot):
         '''
-        Constructor
+        Initialize self properties        
         '''
         self.bot = bot
+        self.driver = self.bot.curr_driver
 
-    def goUrl(self, url):
-        self.bot.currDriver.get(url)
+    def get_url(self, url):
+        """
+        Obtains url passed by param using selenium driver from bot class
+        """
+        self.driver.get(url)
 
-    def goUrl(self, url, waitForLoad=0):
-        self.bot.currDriver.implicitly_wait(waitForLoad)
-        self.bot.currDriver.get(url)
+    def get_url(self, url, waitForLoad=0):
+        """
+        Do get_url including implicit wait for page load
+        """
+        self.driver.implicitly_wait(waitForLoad)
+        self.driver.get(url)
 
-    def getCurrentUrl(self):
-        return self.bot.currDriver.current_url
+    def get_window_handle(self):
+        """
+        Get window object to handle with selenium on scripts
+        """
+        return self.driver.current_window_handle
 
-    def getWindowHandle(self):
-        return self.bot.currDriver.current_window_handle
-
-    def deleteCookie(self, keyName):
+    def delete_cookie_by_key(self, keyName):
         '''
         Deletes a single cookie with the given name.
         '''
-        self.bot.currDriver.delete_cookie(keyName)
+        self.driver.delete_cookie(keyName)
 
-    def deleteCookies(self):
+    def delete_cookies(self):
         '''
         Delete all cookies in the scope of the session.
         '''
-        self.bot.currDriver.delete_all_cookies()
+        self.driver.delete_all_cookies()
 
-    def getCurrentCapabilities(self):
-        return self.bot.currDriver.desired_capabilities
+    def get_capabilities(self):
+        """
+        Retrieve current capabilities applied to selenium driver
+        """
+        return self.driver.desired_capabilities
 
-    def execJs(self, script, webElements):
-        self.bot.currDriver.execute_script(script, webElements)
-    def findElement(self, selector, by=By.CSS_SELECTOR):
+    def execute_js(self, script, webElements):
+        self.driver.execute_script(script, webElements)
+
+    def find_element(self, selector, by=By.CSS_SELECTOR):
         '''
-        Just divided execution ways for the future
+        Just divided execution ways for search web element throught selenium
         '''
         if by == By.CLASS_NAME:
-            return self.bot.currDriver.find_element_by_class_name(selector)
+            return self.driver.find_element_by_class_name(selector)
         elif by == By.CSS_SELECTOR:
-            return self.bot.currDriver.find_element_by_css_selector(selector)
+            return self.driver.find_element_by_css_selector(selector)
         elif by == By.ID:
-            return self.bot.currDriver.find_element_by_id(selector)
+            return self.driver.find_element_by_id(selector)
         elif by == By.LINK_TEXT:
-            return self.bot.currDriver.find_element_by_link_text(selector)
+            return self.driver.find_element_by_link_text(selector)
         elif by == By.NAME:
-            return self.bot.currDriver.find_element_by_name(selector)
+            return self.driver.find_element_by_name(selector)
         elif by == By.PARTIAL_LINK_TEXT:
-            return self.bot.currDriver.find_element_by_partial_link_text(selector)
+            return self.driver.find_element_by_partial_link_text(selector)
         elif by == By.TAG_NAME:
-            return self.bot.currDriver.find_element_by_tag_name(selector)
+            return self.driver.find_element_by_tag_name(selector)
         elif by == By.XPATH:
-            return self.bot.currDriver.find_element_by_xpath(selector)
+            return self.driver.find_element_by_xpath(selector)
+
+    def find_elements(self, selector, by=By.CSS_SELECTOR):
+        '''
+        Just divided execution ways for search web elements throught selenium
+        '''
+        if by == By.CLASS_NAME:
+            return self.driver.find_elements_by_class_name(selector)
+        elif by == By.CSS_SELECTOR:
+            return self.driver.find_elements_by_css_selector(selector)
+        elif by == By.ID:
+            return self.driver.find_elements_by_id(selector)
+        elif by == By.LINK_TEXT:
+            return self.driver.find_elements_by_link_text(selector)
+        elif by == By.NAME:
+            return self.driver.find_elements_by_name(selector)
+        elif by == By.PARTIAL_LINK_TEXT:
+            return self.driver.find_elements_by_partial_link_text(selector)
+        elif by == By.TAG_NAME:
+            return self.driver.find_elements_by_tag_name(selector)
+        elif by == By.XPATH:
+            return self.driver.find_elements_by_xpath(selector)
 
     def forward(self):
-        self.bot.currDriver.forward()
+        """
+        Go forward using browser functionality
+        """
+        self.driver.forward()
 
     def reload(self):
-        self.bot.currDriver.refresh()
+        """
+        Go reloa page using browser functionality
+        """
+        self.driver.refresh()
 
-    def getLog(self, logName='browser'):
+    def get_log(self, log_name='browser'):
         '''
-        Just divided execution ways for the future
+        Get selenium log by name, valid values are
+        + default value : browser
+        + [browser,driver,client, server]        
         '''
-        if logName == 'browser':
-            self.bot.currDriver.get_log(logName)
-        if logName == 'driver':
-            self.bot.currDriver.get_log(logName)
-        if logName == 'client':
-            self.bot.currDriver.get_log(logName)
-        if logName == 'server':
-            self.bot.currDriver.get_log(logName)
+        if log_name == 'browser':
+            self.driver.get_log(log_name)
+        if log_name == 'driver':
+            self.driver.get_log(log_name)
+        if log_name == 'client':
+            self.driver.get_log(log_name)
+        if log_name == 'server':
+            self.driver.get_log(log_name)
 
-    def getScreenshotAsBase64(self):
+    def get_screenshot_as_Base64(self):
         '''
         Gets the screenshot of the current window as a base64 encoded string
         which is useful in embedded images in HTML.
         '''
-        return self.bot.currDriver.get_screenshot_as_base64()
-    def getScreenshotAsFile(self, fileName):
+        return self.driver.get_screenshot_as_base64()
+
+    def get_screenshot_as_file(self, file_name):
         '''
         Gets the screenshot of the current window. Returns False if there is
         any IOError, else returns True. Use full paths in your filename.
+        TODO: support default name
         '''
-        self.bot.currDriver.get_screenshot_as_file(fileName)
+        self.driver.get_screenshot_as_file(file_name)
 
-    def getScreenshotAsPngBin(self, fileName):
+    def get_screenshot_as_png(self):
         '''
         Gets the screenshot of the current window as a binary data.
+        TODO: support default name
         '''
-        return self.bot.currDriver.get_screenshot_as_png()
+        return self.driver.get_screenshot_as_png()
 
-    def getScreenshotSave(self, fileName):
+    def get_screenshot_save(self, file_name):
         '''
         Gets the screenshot of the current window. Returns False if there is
         any IOError, else returns True. Use full paths in your filename.
+        TODO: support default name
         '''
-        self.bot.currDriver.save_screenshot(fileName)
+        return self.driver.save_screenshot(file_name)
 
-    def setJsAsynTimeout(self, timeOut=60):
+    def js_set_timeout(self, timeout=60):
         '''
         Set the amount of time that the script should wait during an
         execute_async_script call before throwing an error.
         '''
-        self.bot.currDriver.set_script_timeout(timeOut)
+        self.driver.set_script_timeout(timeout)
 
-    def setWindowSize(self, x=800, y=600):
+    def set_window_size(self, x=800, y=600):
         '''
         Sets the width and height of the current window. (window.resizeTo)
         '''
-        self.bot.currDriver.set_window_size(x, y)
+        self.driver.set_window_size(x, y)
 
-    def getTitle(self):
+    def get_title(self):
         '''
         Returns the title of the current page.
         '''
-        return self.bot.currDriver.title
+        return self.driver.title
 
-    def setWebElement(self, attrId):
+    def set_web_element(self, new_attr_id):
         '''
         create_web_element
         '''
-        self.bot.currDriver.create_web_element(attrId)
-    def eleClick(self, element=None, selector=None):
+        self.driver.create_web_element(new_attr_id)
+
+    def ele_click(self, element=None, selector=None, by=By.CSS_SELECTOR):
         '''
-        Over element perform web click
-
-        if all params are None, then will raise
-        if Element is not None,then will use element to perform action
-        if Element is None, then will try to use selector search
-            if selector is None , then will raise
-            if selector is not None, then will search element to perform action
-        :param element: WebElement
-        :return: element used to click
+        Perform click over webelement passed by param or search it by default CSS_SELECTOR value
+        if element it's none but selector it's not default value
         '''
-        currEle = None
+        curr_ele = element
+        curr_selector = selector
+        can_click = False
 
-        if element is None and selector is None :
-            raise CoreException('Can\'t click over None element and selector arguments')
-        else:
-            if element is not None :
-                currEle = element
-                element.click()
-                print("Selector it's ignored: {}".format(selector))
-            else:
-                if selector is None:
-                    raise CoreException('Can\'t click over None element and selector arguments')
-                else:
-                    currEle = self.findElement(selector, by=By.CSS_SELECTOR)
-                    currEle.click()
-        return currEle
+        if curr_ele is None and curr_selector is None :            
+            raise CoreException("Can\'t click over None element and None selector arguments: curr_ele={}, curr_selector={}".format(curr_ele,curr_selector))
+        elif curr_ele is None :
+            curr_ele = self.find_element(curr_selector, by=by)                
+            can_click = True
+        elif curr_ele is not None and isinstance(curr_ele, WebElement):            
+            can_click = True
+        if can_click:
+            curr_ele.click()
+        return curr_ele
 
 
-    def eleWrite(self, element, text=None):
+    def ele_write(self, element, text=None):
         '''
         Over element perform send_keys , if not sended text, then will write empty over element
         :param element: WebElement
         :return: None
         '''
-        if text is None :
-            element.send_keys()
+        if not isinstance(element, WebElement):
+            raise CoreException("Element passed by param it's not instance of WebElement class")
+        if text is not None:
+            element.send_keys(text)            
         else:
-            element.send_keys(text)
+            # it's neccessary because some fields shows validation message and color after try to send empty message
+            element.send_keys()
 
-    def getCurrUrl(self):
+    def get_curr_url(self):
         '''
         Return current url from opened bot
         '''
-        return self.bot.currDriver.current_url
+        return self.driver.current_url
