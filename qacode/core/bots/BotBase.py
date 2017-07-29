af8b4e0001272e9613efb1db3c0e3c4cc25f53ee
ntz <netzuleando@gmail.com>
2017-07-29 06:33:48 +0200

    [qacode] FIX bot and botconfig
    + Added documentation
    + Normalized names, ty sonarqube
    + removed useless comments
    + Added functionality for check windows and linux binaries for
      + firefox
      + chrome
      + prhantomjs

--- qacode/core/bots/BotBase.py
+++ qacode/core/bots/BotBase.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 from selenium import webdriver as WebDriver
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver import DesiredCapabilities
@@ -9,110 +9,136 @@ from qacode.core.loggers.LoggerManager import LoggerManager
 
 class BotBase(object):
     '''
-    TODO: implement logger
+    Class Base for handle selenium functionality
+    Properties
+      curr_caps : Capabilities class
+      curr_driver : WebDriver class
+      curr_driver_path : WebDriver browser executable path
+      navigation : Bot methods to brigde selenium functions
+      bot_config : Bot configuration object
+      logger_manager : logger manager class loaded from BotConfig object
+      log : log class to write messages
     '''
-    caps = None # Capabilities class
-    currDriver = None # WebDriver class
-    navs = None # bot methods
-    options = None # bot options
+    curr_caps = None 
+    curr_driver = None 
+    curr_driver_path = None 
+    navigation = None
+    bot_config = None
+    logger_manager = None 
+    log = None 
 
-    loggerManager = None # from options
-    log = None # log
-
-    def __init__(self, options):
-        if options is None:
-            raise Exception('[FAILED]', 'BotBase Options can\'t be none: bad BotOptions provided')
+    def __init__(self, bot_config):
+        """
+        Create new Bot browser based on options object what can be:
+        (help for each option can be found on settings.example.ini)
+        + bot_mode 
+        + bot_browser
+        + bot_url_hub
+        + bot_url_node
+        + bot_profile_path
+        + bot_drivers_path
+        + bot_log_name
+        + bot_log_output_file
+        """
+        if bot_config is None:
+            raise CoreException('[FAILED]', 'BotBase configuration can\'t be none: bad bot_config provided')
         else:
             try:
-                self.options = options
-                self.loggerManager = options.loggerManager
-                self.log = self.loggerManager.get_log()
+                self.bot_config = bot_config
+                self.logger_manager = bot_config.logger_manager
+                self.log = self.logger_manager.get_log()
             except Exception as err:
                 raise CoreException('BotBase, Error: at create LoggerManager', err)
-            self.navs = NavBase(self)  # por implementar
+            self.navigation = NavBase(self) # TODO: testcases
 
-            if self.options.botMode == 'local':
-                self.modeLocal()
-            elif self.options.botMode == 'remote':
-                self.modeRemote()
+            if self.bot_config.bot_mode == 'local':
+                self.mode_local()
+            elif self.bot_config.bot_mode == 'remote':
+                self.mode_remote()
             else:
-                raise Exception('[FAILED]', 'options.mode unkown word | ' + self.options.mode)
+                raise Exception('[FAILED]', 'Unkown word for bot mode config value: {}'.format(self.bot_config.bot_mode))
 
-    def modeLocal(self):
-        self.log.debug('Starting browser with mode : LOCAL')
-        if self.options.botBrowser == 'firefox':
-            self.currCaps = DesiredCapabilities.FIREFOX.copy()
-            # profile = WebDriver.FirefoxProfile.profile_dir = self.options.botProfilePath
-            self.currDriver = WebDriver.Firefox(
-                capabilities=self.currCaps,
-                executable_path="{}/{}".format(self.options.botDriversPath, "geckodriver.exe")
-                )
+    def mode_local(self):
+        """
+        Open new brower on local mode
+        """
+        self.log.info('Starting browser with mode : LOCAL')
+        self.curr_driver_path = "{}/{}".format(self.bot_config.bot_drivers_path)
+
+        if self.bot_config.bot_browser == 'firefox':
+            if os.name == 'nt':
+                self.curr_driver_path.format("geckodriver.exe")
+            else:
+                self.curr_driver_path.format("geckodriver")
+            self.curr_caps = DesiredCapabilities.FIREFOX.copy()
+            self.curr_driver = WebDriver.Firefox(
+                capabilities=self.curr_caps,
+                executable_path= self.curr_driver_path)
 
         elif self.options.botBrowser == 'chrome':
+            if os.name == 'nt':
+                self.curr_driver_path.format("chromedriver.exe")
+            else:
+                self.curr_driver_path.format("chromedriver")
             self.currCaps = DesiredCapabilities.CHROME.copy()
             self.currDriver = WebDriver.Chrome(
-                executable_path="{}/{}".format(self.options.botDriversPath, "chromedriver.exe"),
-                desired_capabilities=self.currCaps)
+                executable_path=self.curr_driver_path,
+                desired_capabilities=self.curr_caps)
 
         elif self.options.botBrowser == 'iexplorer':
             self.currCaps = DesiredCapabilities.INTERNETEXPLORER.copy()
             self.currDriver = WebDriver.Ie(
-                executable_path="{}/{}".format(self.options.botDriversPath, "IEDriverServer_32.exe"),
-                desired_capabilities=self.currCaps)
+                executable_path=self.curr_driver_path.format("IEDriverServer_32.exe"),
+                desired_capabilities=self.curr_caps)
 
         elif self.options.botBrowser == 'edge':
             self.currCaps = DesiredCapabilities.EDGE.copy()
             self.currDriver = WebDriver.Edge(
-                executable_path="{}/{}".format(self.options.botDriversPath, "EdgeDriver_64.exe"),
-                desired_capabilities=self.currCaps)
+                executable_path=self.curr_driver_path.format("EdgeDriver_64.exe"),
+                desired_capabilities=self.curr_caps)
 
         elif self.options.botBrowser == 'phantomjs':
+             if os.name == 'nt':
+                self.curr_driver_path.format("phantomjs.exe")
+            else:
+                self.curr_driver_path.format("phantomjs")
             self.currCaps = DesiredCapabilities.PHANTOMJS.copy()
-            self.currDriver = WebDriver.PhantomJS(
-                executable_path="{}/{}".format(self.options.botDriversPath, "phantomjs.exe"),
-                desired_capabilities=self.currCaps)
+            self.curr_driver = WebDriver.PhantomJS(
+                executable_path=self.curr_driver_path,
+                desired_capabilities=self.curr_caps)
         else:
             raise Exception()
-        self.log.debug('Started browser with mode : LOCAL OK')
+        self.log.info('Started browser with mode : LOCAL OK')
 
-    def modeRemote(self):
-        '''
-        WebDriver.Remote(
-            command_executor,
-            desired_capabilities,
-            browser_profile,
-            proxy,
-            keep_alive,
-            file_detector)
-        '''
-        self.log.debug('Starting browser with mode : REMOTE')
-        if self.options.botBrowser == 'firefox':
-            self.currCaps = DesiredCapabilities.FIREFOX.copy()
-            #profile.accept_untrusted_certs = True
-            #profile.update_preferences()
+    def mode_remote(self):
+        """
+        Open new brower on remote mode
+        """
+        self.log.info('Starting browser with mode : REMOTE')
+        if self.bot_config.bot_browser  == 'firefox':
+            self.curr_caps = DesiredCapabilities.FIREFOX.copy()
 
-        elif self.options.botBrowser == 'chrome':
-            self.currCaps = DesiredCapabilities.CHROME.copy()
+        elif self.bot_config.bot_browser == 'chrome':
+            self.curr_caps= DesiredCapabilities.CHROME.copy()
 
-        elif self.options.botBrowser == 'iexplorer':
-            self.currCaps = DesiredCapabilities.INTERNETEXPLORER.copy()
+        elif self.bot_config.bot_browser  == 'iexplorer':
+            self.curr_caps = DesiredCapabilities.INTERNETEXPLORER.copy()
 
-        elif self.options.botBrowser == 'phantomjs':
-            self.currCaps = DesiredCapabilities.PHANTOMJS.copy()
+        elif self.bot_config.bot_browser == 'phantomjs':
+            self.curr_caps = DesiredCapabilities.PHANTOMJS.copy()
 
-        elif self.options.botBrowser == 'edge':
-            self.currCaps = DesiredCapabilities.EDGE.copy()
-
+        elif self.bot_config.bot_browser == 'edge':
+            self.curr_caps = DesiredCapabilities.EDGE.copy()
         else:
             raise Exception("Bad browser selected")
-        #STARTs REMOTE
-        self.currDriver = RemoteWebDriver(
-            command_executor=self.options.botUrlHub,
-            desired_capabilities=self.currCaps) # browser_profile=profile
-        self.log.debug('Started browser with mode : REMOTE OK')
+
+        self.curr_driver = RemoteWebDriver(
+            command_executor=self.options.bot_url_hub,
+            desired_capabilities=self.curr_caps)
+        self.log.info('Started browser with mode : REMOTE OK')
 
     def close(self):
-        self.log.debug('Closing browser')
-        self.currDriver.close()
-        self.currDriver.quit()
-        self.log.debug('Closed browser OK')
+        self.log.info('Closing browser')
+        self.curr_driver.close()
+        self.curr_driver.quit()
+        self.log.info('Closed browser OK')
---
+++ qacode/core/bots/BotConfig.py
@@ -0,0 +1,61 @@
+import logging
+from testconfig import config as cfg # just works when nose command it's launched
+from qacode.core.loggers.LoggerManager import LoggerManager
+from qacode.core.exceptions.CoreException import CoreException
+
+
+class BotOptions(object):
+    '''
+    Bot configuration for BotBase or inherit classes
+    '''
+    bot_mode = None
+    bot_browser = None
+    bot_url_hub = None
+    bot_url_node = None
+    bot_drivers_path = None
+    bot_log_name = None
+    bot_log_output_file = None
+    logger_manager = None
+    log = None
+
+    def __init__(self, nose_config=cfg, logger_manager=None):
+        """
+        Load all defined options for configure new bots (listed on settings.example.ini)
+        """
+        self.bot_mode = nose_config['BOT']['mode']
+        self.bot_browser = nose_config['BOT']['browser']
+        self.bot_url_hub = nose_config['BOT']['url_hub']
+        self.bot_url_node = nose_config['BOT']['url_node']
+        self.bot_drivers_path = nose_config['BOT']['drivers_path']
+        self.bot_log_name = nose_config['BOT']['log_name']
+        self.bot_log_output_file = nose_config['BOT']['log_output_file']
+                
+        try:
+            self.log_option_loaded(self.bot_mode)
+            self.log_option_loaded(self.bot_browser)
+            self.log_option_loaded(self.bot_url_hub)
+            self.log_option_loaded(self.bot_url_node)
+            self.log_option_loaded(self.bot_profile_path)
+            self.log_option_loaded(self.bot_drivers_path)
+            self.log_option_loaded(self.bot_log_name)
+            self.log_option_loaded(self.bot_log_output_file)                                      
+            self.init_logger_manager(logger_manager)
+        except Exception as err:
+           raise CoreException(err,'Error: at create LoggerManager for  BotConfig class')
+
+    def init_logger_manager(self, logger_manager=None):        
+        """
+        Initialize new logger_manager fot BotConfig object and return it
+        """
+        if logger_manager is None:
+            self.logger_manager = LoggerManager(log_path=self.bot_log_output_file,log_name=self.bot_log_name)            
+        else:
+            self.logger_manager = logger_manager   
+        self.log = self.logger_manager.get_log()
+        return self.logger_manager
+
+    def log_option_loaded(self, option="EMPTY"):
+        """
+        Write log message with bot option value or show EMPTY as value on log
+        """
+        self.log.info('LOADED Bot option : {}'.format(str(option)))
