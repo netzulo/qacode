67fa5b008bfef8f422b47801c3a013e50616f727
crypto netzulo <crypto.netzulo@gmail.com>
2017-08-29 02:42:33 +0200

    [qacode] Updated tests for class TestInfoBase

--- tests/unitaries/TestTestInfoBase.py
+++ tests/unitaries/TestTestInfoBase.py
@@ -5,13 +5,10 @@ from qacode.core.testing.TestInfoBase import TestInfoBase
 class TestTestInfoBase(TestInfoBase):
 
     def __init__(self, method_name="TestTestInfoBase"):
-        super(TestTestInfoBase, self).__init__(method_name, logger_manager=None)
-    
-    def setup(self):
-        super(TestTestInfoBase, self).setup()
+        super(TestTestInfoBase, self).__init__(method_name, logger_manager=None)       
 
-    def tearDown(self):
-        super(TestTestInfoBase, self).tearDown()
-
-    def test_instance(self):
-        self.log.info("message enter setup and tearDown from super TestInfoBase Class")
+    def test_inheritance(self):
+        self.assertIsInstance(self,unittest.TestCase)
+        self.log.info("assertIsInstance : unittest.TestCase class inheritance it's working")
+        self.assertIsInstance(self,TestInfoBase)
+        self.log.info("assertIsInstance : TestInfoBase class inheritance it's working")
