64554bad958b0e9473f6b40b54361069cc09b91a
crypto netzulo <crypto.netzulo@gmail.com>
2017-08-30 03:19:32 +0200

    [qalab] Adding new test to BotBase

--- tests/functionals/TestBotBase.py
+++ tests/functionals/TestBotBase.py
@@ -7,11 +7,23 @@ from qacode.core.bots.BotBase import BotBase
 
 class TestBotBase(TestInfoBase):     
 
-    def __init__(self, method_name="TestTestInfoBase"):
+    def __init__(self, method_name="TestBotBase"):
         super(TestBotBase, self).__init__(method_name, logger_manager=None)
    
     def test_001_bot_local_chrome(self):
-        self.log.debug("TestBotBase: started")
-        self.bot = BotBase(BotConfig(nose_config=cfg, logger_manager=self.logger_manager))
-        self.log.debug("TestBotBase: terminated")
-        
+        self.log.debug("TestBotBase: started for CHROME")
+        bot_config = BotConfig(nose_config=cfg, logger_manager=self.logger_manager)
+        bot_config.bot_browser = "chrome"
+        self.bot = BotBase(bot_config)
+        time.sleep(10)
+        self.bot.close()
+        self.log.debug("TestBotBase: terminated for CHROME")
+    
+    def test_002_bot_local_firefox(self):
+        self.log.debug("TestBotBase: started for FIREFOX")
+        bot_config = BotConfig(nose_config=cfg, logger_manager=self.logger_manager)
+        bot_config.bot_browser = "firefox"
+        self.bot = BotBase(bot_config)
+        time.sleep(10)
+        self.bot.close()
+        self.log.debug("TestBotBase: terminated for FIREFOX")
