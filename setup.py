00cd964b40bed511808e01bf7941f543e20bf2bd
crypto netzulo <crypto.netzulo@gmail.com>
2017-08-31 01:26:44 +0200

    [qacode] UPDATE to v0.0.8

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from os import name
 CURR_PATH = path.abspath(path.dirname(__file__))
 
 setup(name='qacode',
-      version='0.0.7',
+      version='0.0.8',
       packages=find_packages(exclude=['tests']),
       description = 'Main automation lib',
       author = 'Netzulo Open Source',
       author_email = 'netzuleando@gmail.com',
       url = 'https://github.com/netzulo/qacode',
-      download_url = 'https://github.com/netzulo/qacode/tarball/v0.0.7',
+      download_url = 'https://github.com/netzulo/qacode/tarball/v0.0.8',
       keywords = ['testing', 'logging', 'functional','selenium', 'test'],
       install_requires=[
           'appdirs','packaging==16.8','pyparsing',
-          'selenium==3.5.0', '==1.10.0','nose==1.3.7','nose-testconfig==0.10',
+          'selenium==3.5.0','six==1.10.0','nose==1.3.7','nose-testconfig==0.10',
           'TestLink-API-Python-client==0.6.2']
       )
