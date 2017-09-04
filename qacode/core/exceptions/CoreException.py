efe7a1ca33aefba59be829073efeaeda438c7543
crypto netzulo <crypto.netzulo@gmail.com>
2017-09-04 23:28:43 +0200

    [qacode] FIX exceptions params
    issues related
    
    + #37
    + #36

--- qacode/core/exceptions/CoreException.py
+++ qacode/core/exceptions/CoreException.py
@@ -1,14 +1,12 @@
-import logging
-from testconfig import config as cfg # just works when nose command it's launched
 from qacode.core.loggers.LoggerManager import LoggerManager
 
 
 class CoreException(Exception):
     log = None
 
-    def __init__(self, cause, message, log=None):
+    def __init__(self, message, log=None):
         if log is None:
             self.log = LoggerManager().get_log()
         else:
             self.log = log
-        self.log.error("Error at core class: cause={} , message={}".format(cause, message))
+        self.log.error("FAILED qacode.core: message={}".format(cause, message))
