c71041ae5bf5aa196956fc30627d92c09863740d
crypto netzulo <crypto.netzulo@gmail.com>
2017-09-07 02:32:36 +0200

    [qacode] improved tests NavBase

--- tests/functionals/TestNavBase.py
+++ tests/functionals/TestNavBase.py
@@ -9,11 +9,23 @@ class TestNavBase(TestInfoBase):
     def __init__(self, method_name="TestNavBase"):
         super(TestNavBase, self).__init__(method_name, logger_manager=None)
 
-    def test_001_page_base_go_url_param(self):
-        # TODO: open web page throught bot on nav Class
-        self.bot = BotBase(BotConfig(nose_config=cfg))
-        self.bot.navigation.get_url("http://demoqa.com", wait_for_load=10)
-        self.bot.close()
+    def test_001_page_base_go_url_without_wait_param(self):
+        try:
+            self.bot = BotBase(BotConfig(nose_config=cfg))
+            self.bot.navigation.get_url(cfg['TEST_UNITARIES']['url'])
+        except Exception as err:
+            raise Exception(err)
+        finally:
+            self.bot.close()
+
+    def test_001_page_base_go_url_with_wait_param(self):
+        try:
+            self.bot = BotBase(BotConfig(nose_config=cfg))
+            self.bot.navigation.get_url(cfg['TEST_UNITARIES']['url'], wait_for_load=0)
+        except Exception as err:
+            raise Exception(err)
+        finally:
+            self.bot.close()    
 
 
 if __name__ == '__main__':
