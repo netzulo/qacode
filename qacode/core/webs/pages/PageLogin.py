7f4ffcc44da522b78097388d3c331ef7f31c1e1d
crypto netzulo <crypto.netzulo@gmail.com>
2017-09-23 18:21:06 +0200

    [qacode] Added class PageLogin
    + added tests config to settings file
    + FIX for PageBase, ty @terseus
    + Added new Class PageLogin

---
+++ qacode/core/webs/pages/PageLogin.py
@@ -0,0 +1,28 @@
+from qacode.core.webs.pages.PageBase import PageBase
+from qacode.core.exceptions.PageException import PageException
+
+
+class PageLogin(PageBase):
+    """Inherit PageBase class with login and valitation methods"""
+
+    txt_username = None
+    txt_password = None
+    btn_login = None
+
+    def __init__(self,bot, url, by=By.CSS_SELECTOR, selectors=[], go_url=True):
+        super().__init__(bot, url, by ,selectors, go_url)
+        if selectors is None or len(selectors) != 3:
+            raise PageException("Can't generate PageLogin elements if not have 3 selectors")
+        self.txt_username = self.elements[0]
+        self.txt_password = self.elements[1]
+        self.btn_login = self.elements[2]    
+
+    def login(self,username,password,is_login_now=True):
+        message_error = "Can't '{}' '{}' on field '{}' for class PageLogin"
+        try: self.txt_username.send_keys(username)
+        except Exception as err: raise PageException(message_error.format("send_keys", username, "txt_username"))
+        try: self.txt_password.send_keys(password)
+        except Exception as err: raise PageException(message_error.format("send_keys", password, "txt_password"))
+        if is_login_now:
+            try: self.txt_password.send_keys(password)
+            except Exception as err: raise PageException(message_error.format("click", password, "btn_login"))
