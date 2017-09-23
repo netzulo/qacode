f5bf31c96e7033aefe930419d952d2254a711403
crypto netzulo <crypto.netzulo@gmail.com>
2017-09-23 23:52:15 +0200

    [qacode] apply TestInfoBot to all functional Class

--- tests/functionals/TestNavBase.py
+++ tests/functionals/TestNavBase.py
@@ -1,32 +1,24 @@
 import unittest, logging, time
 from testconfig import config as cfg # just works when nose command it's launched
-from qacode.core.testing.TestInfoBase import TestInfoBase
-from qacode.core.bots.BotConfig import BotConfig
-from qacode.core.bots.BotBase import BotBase
+from qacode.core.testing.TesInfoBot import TestInfoBot
 
-class TestNavBase(TestInfoBase):
+class TestNavBase(TestInfoBot):
+    """Test Suite for class NavBase"""
     
     def __init__(self, method_name="TestNavBase"):
         super(TestNavBase, self).__init__(method_name, logger_manager=None)
 
     def test_001_page_base_go_url_without_wait_param(self):
         try:
-            self.bot = BotBase(BotConfig(nose_config=cfg))
             self.bot.navigation.get_url(cfg['TEST_UNITARIES']['url'])
         except Exception as err:
             raise Exception(err)
-        finally:
-            self.bot.close()
 
     def test_001_page_base_go_url_with_wait_param(self):
         try:
-            self.bot = BotBase(BotConfig(nose_config=cfg))
             self.bot.navigation.get_url(cfg['TEST_UNITARIES']['url'], wait_for_load=0)
         except Exception as err:
             raise Exception(err)
-        finally:
-            self.bot.close()    
-
 
 if __name__ == '__main__':
     unittest.main()
--- tests/functionals/TestPageLogin.py
+++ tests/functionals/TestPageLogin.py
@@ -1,13 +1,12 @@
 import unittest, logging, time
 from testconfig import config as cfg # just works when nose command it's launched
-from qacode.core.testing.TestInfoBase import TestInfoBase
-from qacode.core.bots.BotConfig import BotConfig
-from qacode.core.bots.BotBase import BotBase
 from qacode.core.exceptions.PageException import PageException
 from qacode.core.webs.pages.PageLogin import PageLogin
 
+from qacode.core.testing. TesInfoBot import TestInfoBot
 
-class TestPageLogin(TestInfoBase):
+class TestPageLogin(TestInfoBot):
+    """Test Suite for class PageLogin"""
 
     def __init__(self, method_name="TestPageLogin"):
         super(TestPageLogin, self).__init__(method_name, logger_manager=None)
@@ -15,20 +14,16 @@ class TestPageLogin(TestInfoBase):
     def test_001_page_login_instance(self):
         selectors = ["#login", "#password", "[name='commit']"]
         url_login = cfg['TEST_FUNCTIONALS']['url_login']
-        self.bot = BotBase(BotConfig(nose_config=cfg))
         try:
             page = PageLogin(self.bot,url_login, selectors=selectors)
             assert url_login in self.bot.curr_driver.current_url
         except PageException as err:
             self.bot.log.error(err.args)
             raise Exception(err)
-        finally:
-            self.bot.close()
 
     def test_002_page_login_instance_without_selectors(self):
         message_error = "PageLogin must fail at instance without selectors"
         url_login = cfg['TEST_FUNCTIONALS']['url_login']
-        self.bot = BotBase(BotConfig(nose_config=cfg))
         try:
             page = PageLogin(self.bot,url_login)
             self.fail(message_error)
@@ -37,8 +32,6 @@ class TestPageLogin(TestInfoBase):
                 raise Exception(message_error)
             else:
                 self.log.info("PageLogin failed success")
-        finally:
-            self.bot.close()
 
 if __name__ == '__main__':
     unittest.main()
