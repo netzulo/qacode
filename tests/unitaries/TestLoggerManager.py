5ef195919100d351ceef8f55103abe2bb04b75ca
crypto netzulo <crypto.netzulo@gmail.com>
2017-09-24 15:38:03 +0200

    [qacode] FIX unitaries duplicated lanes, #53

--- tests/unitaries/TestLoggerManager.py
+++ tests/unitaries/TestLoggerManager.py
@@ -1,14 +1,17 @@
 import unittest, logging
 from testconfig import config as cfg # just works when nose command it's launched
+from qacode.core.testing.TestInfoBase import TestInfoBase
 from qacode.core.loggers.LoggerManager import LoggerManager
 
+logger_manager = LoggerManager(log_path=cfg["BOT"]["log_output_file"],log_level=logging.DEBUG)
 
-class TestLoggerManager(unittest.TestCase):
+class TestLoggerManager(TestInfoBase):
     '''
     Check LoggerManager class and all logger levels
     '''
-    loggerManager = LoggerManager(log_path=cfg["BOT"]["log_output_file"],log_level=logging.DEBUG)
-    log = loggerManager.get_log()
+    
+    def __init__(self, method_name="TestLoggerManager"):
+        super(TestLoggerManager, self).__init__(method_name, logger_manager=logger_manager)
 
     def test_001_logger_debug(self):        
         """Test: test_001_logger_debug"""
--- tests/unitaries/TestTestInfoBase.py
+++ tests/unitaries/TestTestInfoBase.py
@@ -1,12 +1,15 @@
 import unittest, logging
 from testconfig import config as cfg
 from qacode.core.testing.TestInfoBase import TestInfoBase
+from qacode.core.loggers.LoggerManager import LoggerManager
+
+logger_manager = LoggerManager(log_path=cfg["BOT"]["log_output_file"],log_level=logging.DEBUG)
 
 class TestTestInfoBase(TestInfoBase):
     """Test Suite for TestInfoBase class"""
 
     def __init__(self, method_name="TestTestInfoBase"):
-        super(TestTestInfoBase, self).__init__(method_name, logger_manager=None)
+        super(TestTestInfoBase, self).__init__(method_name, logger_manager=logger_manager)
 
     def test_001_inheritance(self):
         """Test: test_001_inheritance"""
