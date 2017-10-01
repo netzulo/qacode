aa3d8c56d14482d1ab1e21a807def561ea572ba3
crypto netzulo <crypto.netzulo@gmail.com>
2017-10-01 16:12:54 +0200

    [qacode] pylint fix for Utils.py

--- qacode/core/utils/Utils.py
+++ qacode/core/utils/Utils.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+Utils tasks
+- files operations
+- settings operations
+"""
 
 
 from sys import version_info
@@ -7,12 +12,12 @@ import json
 
 
 def get_path_join(file_path=None, file_name=None):
-        """Return absolute path for __file__ instance"""
-        if file_path is None or not path.exists(file_path):
-            raise IOError("Path '{0!s}' doesn't exists")
-        if file_name is None or not path.exists(file_name):
-            raise IOError("File '{0!s}' doesn't exists")
-        return path.join(file_path, file_name)
+    """Return absolute path for __file__ instance"""
+    if file_path is None or not path.exists(file_path):
+        raise IOError("Path '{0!s}' doesn't exists")
+    if file_name is None or not path.exists(file_name):
+        raise IOError("File '{0!s}' doesn't exists")
+    return path.join(file_path, file_name)
 
 def read_file(is_json=False, file_path=None, encoding='utf-8', is_encoding=True):
     """Returns file object from file_path,
@@ -22,24 +27,23 @@ def read_file(is_json=False, file_path=None, encoding='utf-8', is_encoding=True)
       can modify encoding used to obtain file
     """
     if is_json:
-        return json.load(file_abspath)
+        return json.load(file_path)
     if file_path is None:
         raise Exception("File path received it's None")
     if version_info.major >= 3:
         if not is_encoding:
             encoding = None
-        with open(file_path, encoding=encoding) as f:
-            return f.read()
+        with open(file_path, encoding=encoding) as buff:
+            return buff.read()
     if version_info.major <= 2:
-        with open(file_path) as f:
+        with open(file_path) as buff:
             if is_encoding:
-                return f.read().decode(encoding)
+                return buff.read().decode(encoding)
             else:
-                return f.read()
+                return buff.read()
 
 def settings():
     """Returns file settings as a dict to be use on qacode lib"""
-    return read_file(is_json=True, file_path=get_path_join(
-        file_path='qacode/configs/',
-        file_name='settings.ini')
-    )
+    return read_file(is_json=True,
+                     file_path=get_path_join(file_path='qacode/configs/',
+                                             file_name='settings.json'))
