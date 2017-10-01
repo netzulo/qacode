dfa7a72b9ff34e260e2053dea5cffae3ed348e89
crypto netzulo <crypto.netzulo@gmail.com>
2017-10-02 01:19:08 +0200

    [qacode]pylint fix for LoggerManager+improves

--- qacode/core/loggers/LoggerManager.py
+++ qacode/core/loggers/LoggerManager.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
+"""Module related with all logging tasks"""
 
 
 import logging
-import os
+from qacode.core.utils.Utils import get_path_join
 
 
 class LoggerManager(object):
     '''
-    @use: self.get_log()
-    TODO: convertir constructor a gestor de loggers
-    TODO: cambiar el codigo del constructor que no sea validacion de datos
-          hacia una funcion create
+    Manager to instance configurated logger
+    @usage: instance.get_log()
     '''
+
+    log_path = None
+    log_name = None
+    log_path_join = None
+    log_level = None
+
 
     def __init__(self,
                  log_path="logs",
@@ -20,39 +25,47 @@ class LoggerManager(object):
         if len(log_path) <= 0:
             raise Exception("bad format at logger log_path={}"
                             .format(log_path))
+        self.log_path = log_path
 
-        if len(log_name) <= 0 and log_name != "qalab-core":
-            raise Exception("bad log name provided , log_path={}"
-                            .format(log_path))
+        if log_name is None or len(log_name) <= 0:
+            raise Exception("Log name not provided: log_name={}"
+                            .format(log_name))
+        self.log_name = log_name
 
         if log_level is None:
-            log_level = logging.DEBUG
-
+            self.log_level = logging.DEBUG
+        else:
+            self.log_level = log_level
         if not is_output_console and not is_output_file:
             raise Exception(
                 "Can't start LoggerManager without any handler, "
-                "is_output_console={} , is_output_file="
+                "is_output_console={} , is_output_file={}"
                 .format(is_output_console, is_output_file)
             )
+        self.is_output_console = is_output_console
+        self.is_output_file = is_output_file
+        self.log_path_join = get_path_join(
+            file_path=self.log_path,
+            file_name=self.log_name)
+        self.create_logger()
 
-        if os.name == "nt":
-            self.log_file_path = "{}\\{}.log".format(log_path, log_name)
-        else:
-            self.log_file_path = "{}/{}.log".format(log_path, log_name)
+    def create_logger(self):
+        """TODO"""
 
-        self.logger = logging.getLogger(log_name)
-        self.logger.setLevel(log_level)
+        self.logger = logging.getLogger(self.log_name)
+        self.logger.setLevel(self.log_level)
         self.log_formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         )
-        if is_output_console:
+        if self.is_output_console:
             self.logger.addHandler(self.create_console_handler(
-                log_level, self.log_formatter
+                self.log_level, self.log_formatter
             ))
-        if is_output_file:
+        if self.is_output_file:
             self.logger.addHandler(self.create_file_handler(
-                log_level, self.log_formatter, self.log_file_path
+                self.log_level, self.log_formatter, self.log_path_join
             ))
+
 
     def create_console_handler(self, log_level, log_formatter):
         """create console handler and set logfile level"""
@@ -61,12 +74,13 @@ class LoggerManager(object):
         console_handler.setFormatter(log_formatter)
         return console_handler
 
-    def create_file_handler(self, log_level, log_formatter, log_file_path):
+    def create_file_handler(self, log_level, log_formatter, log_path_join):
         """create console handler and set logfile level"""
-        file_handler = logging.FileHandler(log_file_path)
+        file_handler = logging.FileHandler(log_path_join)
         file_handler.setLevel(log_level)
         file_handler.setFormatter(log_formatter)
         return file_handler
 
     def get_log(self):
+        """Returns property self.logger"""
         return self.logger
