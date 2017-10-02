1e9a7cba29e705cd25a3bade2ec04419ea23d47a
crypto netzulo <crypto.netzulo@gmail.com>
2017-10-02 03:18:36 +0200

    [qacode]fix pylint TestBotBase+TestConfig

--- tests/functionals/TestBotBase.py
+++ tests/functionals/TestBotBase.py
@@ -1,151 +1,162 @@
 # -*- coding: utf-8 -*-
+"""TODO"""
 
 
 import unittest
-import logging
+
 import time
+
 from qacode.core.testing.TestInfoBase import TestInfoBase
 from qacode.core.bots.BotConfig import BotConfig
 from qacode.core.bots.BotBase import BotBase
 from qacode.core.loggers.LoggerManager import LoggerManager
-from testconfig import config as cfg
+from qacode.core.utils.Utils import settings
 
 
-logger_manager = LoggerManager(
-    log_path=cfg["BOT"]["log_output_file"],
-    log_level=logging.DEBUG
-)
+LOGGER_MANAGER = LoggerManager()
+CONFIG = settings()
 
 
 class TestBotBase(TestInfoBase):
+    """Tests for class BotBase"""
 
     def __init__(self, method_name="TestBotBase"):
         super(TestBotBase, self).__init__(
-            method_name, logger_manager=logger_manager
+            method_name, logger_manager=LOGGER_MANAGER
         )
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support local drivers")
     def test_001_bot_local_chrome(self):
+        """Testcase: test_001_bot_local_chrome"""
         self.log.debug("TestBotBase: LOCAL started for CHROME")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "chrome"
         bot_config.bot_mode = "local"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: LOCAL terminated for CHROME")
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support local drivers")
     def test_002_bot_local_firefox(self):
+        """Testcase: test_002_bot_local_firefox"""
         self.log.debug("TestBotBase: LOCAL started for FIREFOX")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "firefox"
         bot_config.bot_mode = "local"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: LOCAL terminated for FIREFOX")
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support local drivers")
     def test_003_bot_local_phantomjs(self):
+        """Testcase: test_003_bot_local_phantomjs"""
         self.log.debug("TestBotBase: LOCAL started for PHANTOMJS")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "phantomjs"
         bot_config.bot_mode = "local"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: LOCAL terminated for PHANTOMJS")
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support local drivers")
     def test_004_bot_local_iexplorer(self):
+        """Testcase: test_004_bot_local_iexplorer"""
         self.log.debug("TestBotBase: LOCAL started for IEXPLORER")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "iexplorer"
         bot_config.bot_mode = "local"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: LOCAL terminated for IEXPLORER")
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support local drivers")
     def test_005_bot_local_edge(self):
+        """Testcase: test_005_bot_local_edge"""
         self.log.debug("TestBotBase: LOCAL started for EDGE")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "edge"
         bot_config.bot_mode = "local"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: LOCAL terminated for EDGE")
 
     def test_006_bot_remote_chrome(self):
+        """Testcase: test_006_bot_remote_chrome"""
         self.log.debug("TestBotBase: REMOTE started for CHROME")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "chrome"
         bot_config.bot_mode = "remote"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: REMOTE terminated for CHROME")
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support remote firefox drivers")
     def test_007_bot_remote_firefox(self):
+        """Testcase: test_007_bot_remote_firefox"""
         self.log.debug("TestBotBase: REMOTE started for FIREFOX")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "firefox"
         bot_config.bot_mode = "remote"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: REMOTE terminated for FIREFOX")
 
     def test_008_bot_remote_phantomjs(self):
+        """Testcase: test_008_bot_remote_phantomjs"""
         self.log.debug("TestBotBase: REMOTE started for PHANTOMJS")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "phantomjs"
         bot_config.bot_mode = "remote"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: REMOTE terminated for PHANTOMJS")
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support remote windows drivers")
     def test_009_bot_remote_iexplorer(self):
+        """Testcase: test_009_bot_remote_iexplorer"""
         self.log.debug("TestBotBase: REMOTE started for IEXPLORER")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "iexplorer"
         bot_config.bot_mode = "remote"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: REMOTE terminated for IEXPLORER")
 
-    @unittest.skipIf(cfg["BUILD"]["skip_travis_tests"],
+    @unittest.skipIf(CONFIG["BUILD"]["skip_travis_tests"],
                      "TRAVIS build not support remote windows drivers")
     def test_010_bot_remote_edge(self):
+        """Testcase: test_010_bot_remote_edge"""
         self.log.debug("TestBotBase: REMOTE started for EDGE")
-        bot_config = BotConfig(nose_config=cfg,
+        bot_config = BotConfig(nose_config=self.test_config,
                                logger_manager=self.logger_manager)
         bot_config.bot_browser = "edge"
         bot_config.bot_mode = "remote"
-        self.bot = BotBase(bot_config)
+        bot = BotBase(bot_config)
         time.sleep(10)
-        self.bot.close()
+        bot.close()
         self.log.debug("TestBotBase: REMOTE terminated for EDGE")
--- tests/unitaries/TestConfig.py
+++ tests/unitaries/TestConfig.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
+"""TODO"""
 
 
 import os
-import logging
 import ast
-from testconfig import config as cfg
+
 from qacode.core.testing.TestInfoBase import TestInfoBase
 from qacode.core.loggers.LoggerManager import LoggerManager
 
-
-logger_manager = LoggerManager(
-    log_path=cfg["BOT"]["log_output_file"],
-    log_level=logging.DEBUG
-)
-
-
-# TODO: Replace literal strings with constants where it makes sense
+LOGGER_MANAGER = LoggerManager()
 class TestConfig(TestInfoBase):
     '''
-    Comprueba el fichero que se pasa por parametro a Nose, si contiene un JSON
-    con la estructura pedida en los tests
+    TestSuite for settings keys and values
     '''
+
+    # Error Messages
+    ERR_KEY_NOT_FOUND = "Required key '{}', can't be None"
+    ERR_KEY_INVALID_VALUE = "Required key '{}', just can be in '{}'"
+    ERR_KEY_PATH_NOT_FOUND = "Required key '{}', not found for path '{}'"
+    ERR_KEY_EMPTY = "Required key '{}', can't be empty, value='{}'",
+    ERR_KEY_REGEX = "Optional key '{}', not provided or not matching regex: {} ",
+    # Test constants
+    PATH_SETTINGS = "qacode/configs/settings.json"
     # TODO: Replace this list with a dict and stop using numeric indexes
     msgs = [
         "Settings file doesn't found, copy from settings.example.ini",
@@ -59,54 +60,65 @@ class TestConfig(TestInfoBase):
 
     def __init__(self, method_name="TestConfig"):
         super(TestConfig, self).__init__(
-            method_name, logger_manager=logger_manager
+            method_name, logger_manager=LOGGER_MANAGER
         )
 
     def test_000_config_exist(self):
         """Test : test_000_config_exist"""
-        exist = os.path.exists("qacode/configs/settings.ini")
-        self.assertEqual(exist, True, self.msgs[0])
+        self.assertEqual(True, os.path.exists(self.PATH_SETTINGS),
+                         self.ERR_KEY_PATH_NOT_FOUND.format("Log file", self.PATH_SETTINGS))
 
-    def test_001_config_nose_loaded(self):
+    def test_001_config_loaded(self):
         """Test : test_001_config_nose_loaded"""
-        sections = [cfg["BOT"], cfg["TESTLINK"], cfg["TEST_UNITARIES"]]
+        sections = [
+            self.test_config['bot'],
+            self.test_config['testlink'],
+            self.test_config['tests'],
+            self.test_config['tests']['unitaries'],
+            self.test_config['tests']['functionals'],
+            self.test_config['build'],
+        ]
         self.assertNotIn(None, sections, self.msgs[1])
 
-    def test_002_config_has_key_bot_mode(self):
-        """Test : test_002_config_has_key_bot_mode"""
+    def test_002_botmode(self):
+        """Test : Has key bot.mode with valid value"""
+        value = self.test_config['bot']['mode']
         valid_values = ["local", "remote"]
-        self.assertIn(cfg.get("BOT")["mode"], valid_values, self.msgs[2])
+        self.assertIn(value,valid_values,
+                      self.ERR_KEY_INVALID_VALUE.format('bot.mode',value))
 
-    def test_003_config_has_key_bot_browser(self):
-        """Test : test_003_config_has_key_bot_browser"""
+    def test_003_botbrowser(self):
+        """Test : Has key bot.browser with valid value"""
+        value = self.test_config['bot']["browser"]
         valid_values = ["firefox", "chrome", "iexplorer", "phantomjs"]
-        self.assertIn(cfg.get("BOT")["browser"], valid_values, self.msgs[3])
+        self.assertIn(value, valid_values,
+                      self.ERR_KEY_INVALID_VALUE.format('bot.mode',value))
 
-    def test_004_config_has_key_bot_url_hub(self):
+    def test_004_config_has_key_bot_url_hub(self): # TODO: edit test
         """Test : test_004_config_has_key_bot_url_hub"""
-        self.assertRegexpMatches(
-            cfg.get("BOT")["url_hub"], self.regexs[0],
+        self.assertRegex(
+            self.test_config['bot']["url_hub"], self.regexs[0],
             self.msgs[4].format(self.regexs[0])
         )
 
-    def test_005_config_has_key_bot_url_node(self):
+    def test_005_config_has_key_bot_url_node(self): # TODO: edit test
         """Test : test_005_config_has_key_bot_url_node"""
-        self.assertRegexpMatches(
-            cfg.get("BOT")["url_node"], self.regexs[0],
+        self.assertRegex(
+            self.test_config['bot']["url_node"], self.regexs[0],
             self.msgs[5].format(self.regexs[0])
         )
 
-    def test_006_config_has_key_bot_drivers_path(self):
+    def test_006_config_has_key_bot_drivers_path(self): # TODO: edit test
         """Test : test_006_config_has_key_bot_drivers_path"""
-        value = cfg.get("BOT")["drivers_path"]
+        value = self.test_config['bot']["drivers_path"]
         exist = os.path.exists(value)
         if not exist:
             self.log.warn(self.msgs[7])
 
-    def test_007_config_has_key_bot_drivers_names(self):
+    def test_007_config_has_key_bot_drivers_names(self): # TODO: edit test
         """Test : test_007_config_has_key_bot_drivers_names"""
-        values = ast.literal_eval(cfg.get("BOT")["drivers_names"])
-        file_path = "{}{}{}".format(cfg.get("BOT")["drivers_path"], "{}", "{}")
+        values = ast.literal_eval(self.test_config['bot']["drivers_names"])
+        file_path = "{}{}{}".format(self.test_config['bot']["drivers_path"], "{}", "{}")
         if os.name == "nt":
             file_path = file_path.format("\\", "{}")
         else:
@@ -117,88 +129,88 @@ class TestConfig(TestInfoBase):
             if not exist:
                 self.log.warn(self.msgs[8].format(driver_name))
 
-    def test_008_config_has_key_bot_log_name(self):
+    def test_008_config_has_key_bot_log_name(self): # TODO: edit test
         """Test : test_008_config_has_key_bot_log_name"""
-        value = cfg.get("BOT")["log_name"]
+        value = self.test_config['bot']["log_name"]
         self.assertNotEqual(value, "", self.msgs[9])
 
-    def test_009_config_has_key_bot_log_output_file(self):
+    def test_009_config_has_key_bot_log_output_file(self): # TODO: edit test
         """Test : test_009_config_has_key_bot_log_output_file"""
-        value = cfg.get("BOT")["log_output_file"]
+        value = self.test_config['bot']["log_output_file"]
         self.assertNotEqual(value, "", self.msgs[10])
 
-    def test_010_config_has_key_testlink_url(self):
+    def test_010_config_has_key_testlink_url(self): # TODO: edit test
         """Test : test_010_config_has_key_testlink_url"""
-        value = cfg.get("TESTLINK")["url"]
+        value = self.test_config.get("TESTLINK")["url"]
         exist_length = len(value)
         if exist_length <= 0:
             self.log.warn(self.msgs[11].format(self.regexs[0]))
         else:
             self.assertRegexpMatches(value, self.regexs[0], self.msgs[11])
 
-    def test_011_config_has_key_testlink_devkey(self):
+    def test_011_config_has_key_testlink_devkey(self): # TODO: edit test
         """Test : test_011_config_has_key_testlink_devkey"""
-        value = cfg.get("TESTLINK")["devkey"]
+        value = self.test_config.get("TESTLINK")["devkey"]
         exist_length = len(value)
         if exist_length <= 0:
             self.log.warn(self.msgs[12])
 
-    def test_012_config_has_key_test_unitaries_url(self):
+    def test_012_config_has_key_test_unitaries_url(self): # TODO: edit test
         """Test : test_012_config_has_key_test_unitaries_url"""
         self.assertRegexpMatches(
-            cfg.get("TEST_UNITARIES")["url"],
+            self.test_config.get("TEST_UNITARIES")["url"],
             self.regexs[0], self.msgs[13]
         )
 
-    def test_013_config_has_key_test_functionals_url_login(self):
+    def test_013_config_has_key_test_functionals_url_login(self): # TODO: edit test
         """Test : test_013_config_has_key_test_functionals_url_login"""
         self.assertRegexpMatches(
-            cfg.get("TEST_FUNCTIONALS")["url_login"],
+            self.test_config.get("TEST_FUNCTIONALS")["url_login"],
             self.regexs[0], self.msgs[14]
         )
 
-    def test_014_config_has_key_test_functionals_url_logout(self):
+    def test_014_config_has_key_test_functionals_url_logout(self): # TODO: edit test
         """Test : test_014_config_has_key_test_functionals_url_logout"""
         self.assertRegexpMatches(
-            cfg.get("TEST_FUNCTIONALS")["url_logout"],
+            self.test_config.get("TEST_FUNCTIONALS")["url_logout"],
             self.regexs[0], self.msgs[15]
         )
 
-    def test_015_config_has_key_test_functionals_url_logged_ok(self):
+    def test_015_config_has_key_test_functionals_url_logged_ok(self): # TODO: edit test
         """Test : test_015_config_has_key_test_functionals_url_logged_ok"""
         self.assertRegexpMatches(
-            cfg.get("TEST_FUNCTIONALS")["url_logged_ok"],
+            self.test_config.get("TEST_FUNCTIONALS")["url_logged_ok"],
             self.regexs[0], self.msgs[16]
         )
 
-    def test_016_config_has_key_test_functionals_url_logged_ko(self):
+    def test_016_config_has_key_test_functionals_url_logged_ko(self): # TODO: edit test
         """Test : test_016_config_has_key_test_functionals_url_logged_ko"""
         self.assertRegexpMatches(
-            cfg.get("TEST_FUNCTIONALS")["url_logged_ko"],
+            self.test_config.get("TEST_FUNCTIONALS")["url_logged_ko"],
             self.regexs[0], self.msgs[17]
         )
 
-    def test_017_config_has_key_test_functionals_selectors_login(self):
+    def test_017_config_has_key_test_functionals_selectors_login(self): # TODO: edit test
         """Test : test_017_config_has_key_test_functionals_selectors_login"""
         values = ast.literal_eval(
-            cfg.get("TEST_FUNCTIONALS")["selectors_login"]
+            self.test_config.get("TEST_FUNCTIONALS")["selectors_login"]
         )
         self.assertEqual(len(values), 3, self.msgs[18])
         for value in values:
             self.assertNotEqual(value, "", self.msgs[18])
 
-    def test_018_config_has_key_test_functionals_creed_user(self):
+    def test_018_config_has_key_test_functionals_creed_user(self): # TODO: edit test
         """Test : test_018_config_has_key_test_functionals_creed_user"""
-        value = cfg.get("TEST_FUNCTIONALS")["creed_user"]
+        value = self.test_config.get("TEST_FUNCTIONALS")["creed_user"]
         self.assertNotEqual(value, "", self.msgs[19])
 
-    def test_019_config_has_key_test_functionals_creed_pass(self):
+    def test_019_config_has_key_test_functionals_creed_pass(self): # TODO: edit test
         """Test : test_019_config_has_key_test_functionals_creed_pass"""
-        value = cfg.get("TEST_FUNCTIONALS")["creed_pass"]
+        value = self.test_config.get("TEST_FUNCTIONALS")["creed_pass"]
         self.assertNotEqual(value, "", self.msgs[20])
 
-    def test_020_config_has_key_build_skip_travis_tests(self):
+    def test_020_config_has_key_build_skip_travis_tests(self): # TODO: edit test
         """Test : test_020_config_has_key_build_skip_travis_tests"""
-        value = cfg.get("BUILD")["skip_travis_tests"]
+        value = self.test_config.get("BUILD")["skip_travis_tests"]
         self.assertNotEqual(value, None, self.msgs[21])
         self.assertNotEqual(value, "", self.msgs[21])
