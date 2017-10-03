9ae214fce8f8926ffe4513947571e1c87f902983
crypto netzulo <crypto.netzulo@gmail.com>
2017-10-04 00:00:43 +0200

    [qacode]pylint fixes for CoreException

--- qacode/core/exceptions/CoreException.py
+++ qacode/core/exceptions/CoreException.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
+"""Main Exceptions for qacode library"""
 
 
 from qacode.core.loggers.LoggerManager import LoggerManager
 
 
 class CoreException(Exception):
+    """Base Exception class for inherit new exceptions on library"""
     log = None
 
-    def __init__(self, message, log=None):
+    def __init__(self, err=None, message='Exception without message', log=None):
+        """Raise an exception from any part of qacode package"""
+        super(CoreException, self).__init__(err, message)
+        msg = "FAILED qacode.core: message={}"
         if log is None:
-            self.log = LoggerManager(log_path="logs").get_log()
+            self.log = LoggerManager().get_log()
         else:
             self.log = log
-        self.log.error("FAILED qacode.core: message={}".format(message))
+        self.log.error(str(msg.format(message)))
