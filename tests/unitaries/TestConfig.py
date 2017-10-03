824a36e28e9bd1b2456aaafafdf667c8f06f0987
crypto netzulo <crypto.netzulo@gmail.com>
2017-10-03 20:40:38 +0200

    [qacode] pylint fix for TestConfig.py

--- tests/unitaries/TestConfig.py
+++ tests/unitaries/TestConfig.py
@@ -3,10 +3,10 @@
 
 
 import os
-import ast
 
 from qacode.core.testing.TestInfoBase import TestInfoBase
 from qacode.core.loggers.LoggerManager import LoggerManager
+from qacode.core.utils.Utils import path_format
 
 LOGGER_MANAGER = LoggerManager()
 class TestConfig(TestInfoBase):
@@ -18,45 +18,11 @@ class TestConfig(TestInfoBase):
     ERR_KEY_NOT_FOUND = "Required key '{}', can't be None"
     ERR_KEY_INVALID_VALUE = "Required key '{}', just can be in '{}'"
     ERR_KEY_PATH_NOT_FOUND = "Required key '{}', not found for path '{}'"
-    ERR_KEY_EMPTY = "Required key '{}', can't be empty, value='{}'",
-    ERR_KEY_REGEX = "Optional key '{}', not provided or not matching regex: {} ",
+    ERR_KEY_EMPTY = "Required key '{}', can't be empty, value='{}'"
+    ERR_KEY_REGEX = "Optional key '{}', not provided or not matching regex: {} "
     # Test constants
     PATH_SETTINGS = "qacode/configs/settings.json"
-    # TODO: Replace this list with a dict and stop using numeric indexes
-    msgs = [
-        "Settings file doesn't found, copy from settings.example.ini",
-        "Some missing section on settings.ini file",
-        "BOT mode just can be : local,remote",
-        "BOT browser just can be : firefox , chrome , iexplorer, phantomjs",
-        "BOT url_hub just can be match with this regular expression : {}",
-        "BOT url_node just can be match with this regular expression : {}",
-        "BOT profile_path, optional key: file not found or not provided",
-        "BOT drivers_path: path not found or not provided",
-        "BOT drivers_names: path not found for driver_name={}",
-        "BOT log_name can't be empty name",
-        "BOT log_output_file can't be empty name",
-        ("TESTLINK url, optional key, not provided or not matching regular "
-         "expression: {} "),
-        "TESTLINK devkey, optional key: file not found",
-        ("TEST_UNITARIES url just can be match with this regular expression : "
-         "{}"),
-        ("TEST_FUNCTIONALS url_login just can be match with this regular "
-         "expression : {}"),
-        ("TEST_FUNCTIONALS url_logout just can be match with this regular "
-         "expression : {}"),
-        ("TEST_FUNCTIONALS url_logged_ok just can be match with this regular "
-         "expression : {}"),
-        ("TEST_FUNCTIONALS url_logged_ko just can be match with this regular "
-         "expression : {}"),
-        ("TEST_FUNCTIONALS selectors can't be empty array and can't contain "
-         "empty selectors"),
-        "TEST_FUNCTIONALS creed_user, can't be empty string",
-        "TEST_FUNCTIONALS creed_pass, can't be empty string",
-        "BUILD skip_travis_tests, can't be None, just bool values",
-    ]
-    regexs = [
-        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'  # noqa
-    ]
+    REGEX_URL = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
     def __init__(self, method_name="TestConfig"):
         super(TestConfig, self).__init__(
@@ -68,149 +34,146 @@ class TestConfig(TestInfoBase):
         self.assertEqual(True, os.path.exists(self.PATH_SETTINGS),
                          self.ERR_KEY_PATH_NOT_FOUND.format("Log file", self.PATH_SETTINGS))
 
-    def test_001_config_loaded(self):
-        """Test : test_001_config_nose_loaded"""
-        sections = [
-            self.test_config['bot'],
-            self.test_config['testlink'],
-            self.test_config['tests'],
-            self.test_config['tests']['unitaries'],
-            self.test_config['tests']['functionals'],
-            self.test_config['build'],
-        ]
-        self.assertNotIn(None, sections, self.msgs[1])
-
-    def test_002_botmode(self):
+    def test_001_bot_mode(self):
         """Test : Has key bot.mode with valid value"""
         value = self.test_config['bot']['mode']
         valid_values = ["local", "remote"]
-        self.assertIn(value,valid_values,
-                      self.ERR_KEY_INVALID_VALUE.format('bot.mode',value))
+        self.assertIn(value, valid_values,
+                      self.ERR_KEY_INVALID_VALUE.format('bot.mode', value))
 
-    def test_003_botbrowser(self):
+    def test_002_bot_browser(self):
         """Test : Has key bot.browser with valid value"""
         value = self.test_config['bot']["browser"]
         valid_values = ["firefox", "chrome", "iexplorer", "phantomjs"]
         self.assertIn(value, valid_values,
-                      self.ERR_KEY_INVALID_VALUE.format('bot.mode',value))
+                      self.ERR_KEY_INVALID_VALUE.format('bot.mode', value))
 
-    def test_004_config_has_key_bot_url_hub(self): # TODO: edit test
-        """Test : test_004_config_has_key_bot_url_hub"""
+    def test_003_bot_url_hub(self):
+        """Test : Has key bot.url_hub and match regex"""
         self.assertRegex(
-            self.test_config['bot']["url_hub"], self.regexs[0],
-            self.msgs[4].format(self.regexs[0])
+            self.test_config['bot']["url_hub"], self.REGEX_URL,
+            self.ERR_KEY_REGEX.format('bot.url_hub', self.REGEX_URL)
         )
 
-    def test_005_config_has_key_bot_url_node(self): # TODO: edit test
+    def test_004_bot_url_node(self):
         """Test : test_005_config_has_key_bot_url_node"""
         self.assertRegex(
-            self.test_config['bot']["url_node"], self.regexs[0],
-            self.msgs[5].format(self.regexs[0])
+            self.test_config['bot']["url_node"], self.REGEX_URL,
+            self.ERR_KEY_REGEX.format('bot.url_node', self.REGEX_URL)
         )
 
-    def test_006_config_has_key_bot_drivers_path(self): # TODO: edit test
+    def test_005_bot_drivers_path(self):
         """Test : test_006_config_has_key_bot_drivers_path"""
         value = self.test_config['bot']["drivers_path"]
-        exist = os.path.exists(value)
-        if not exist:
-            self.log.warn(self.msgs[7])
+        msg = self.ERR_KEY_PATH_NOT_FOUND.format('bot.drivers_path', value)
+        if not os.path.exists(value):
+            self.log.warning(msg)
 
-    def test_007_config_has_key_bot_drivers_names(self): # TODO: edit test
+    def test_006_bot_drivers_names(self):
         """Test : test_007_config_has_key_bot_drivers_names"""
-        values = ast.literal_eval(self.test_config['bot']["drivers_names"])
-        file_path = "{}{}{}".format(self.test_config['bot']["drivers_path"], "{}", "{}")
-        if os.name == "nt":
-            file_path = file_path.format("\\", "{}")
-        else:
-            file_path = file_path.format("/", "{}")
+        values = self.test_config['bot']["drivers_names"]
         for driver_name in values:
-            file_path = file_path.format(driver_name)
-            exist = os.path.exists(file_path)
-            if not exist:
-                self.log.warn(self.msgs[8].format(driver_name))
+            file_path = path_format(
+                self.test_config['bot']["drivers_path"],
+                driver_name)
+            msg = self.ERR_KEY_PATH_NOT_FOUND.format('bot.drivers_names', file_path)
 
-    def test_008_config_has_key_bot_log_name(self): # TODO: edit test
+            if not os.path.exists(file_path):
+                self.log.warning(msg)
+
+    def test_007_bot_log_name(self):
         """Test : test_008_config_has_key_bot_log_name"""
         value = self.test_config['bot']["log_name"]
-        self.assertNotEqual(value, "", self.msgs[9])
+        self.assertNotEqual(value, "", self.ERR_KEY_EMPTY.format(
+            'bot.log_name', value))
 
-    def test_009_config_has_key_bot_log_output_file(self): # TODO: edit test
+    def test_008_bot_log_output_file(self):
         """Test : test_009_config_has_key_bot_log_output_file"""
         value = self.test_config['bot']["log_output_file"]
-        self.assertNotEqual(value, "", self.msgs[10])
+        self.assertNotEqual(value, "", self.ERR_KEY_EMPTY.format(
+            'bot.log_output_file', value))
 
-    def test_010_config_has_key_testlink_url(self): # TODO: edit test
+    def test_009_testlink_url_api(self):
         """Test : test_010_config_has_key_testlink_url"""
-        value = self.test_config.get("TESTLINK")["url"]
-        exist_length = len(value)
-        if exist_length <= 0:
-            self.log.warn(self.msgs[11].format(self.regexs[0]))
-        else:
-            self.assertRegexpMatches(value, self.regexs[0], self.msgs[11])
+        value = self.test_config['testlink']["url_api"]
+        msg = self.ERR_KEY_REGEX.format('testlink.url_api', self.REGEX_URL)
+        if len(value) <= 0:
+            self.log.warning(msg)
+        else: # optional key
+            self.assertRegex(value, self.REGEX_URL, msg)
 
-    def test_011_config_has_key_testlink_devkey(self): # TODO: edit test
+    def test_010_testlink_devkey(self):
         """Test : test_011_config_has_key_testlink_devkey"""
-        value = self.test_config.get("TESTLINK")["devkey"]
-        exist_length = len(value)
-        if exist_length <= 0:
-            self.log.warn(self.msgs[12])
+        value = self.test_config['testlink']["dev_key"]
+        msg = self.ERR_KEY_EMPTY.format('testlink.testlink', value)
+        if len(value) <= 0:
+            self.log.warning(msg)
+        else: # optional key
+            self.assertNotEqual(value, "", msg)
 
-    def test_012_config_has_key_test_unitaries_url(self): # TODO: edit test
+    def test_011_key_url(self):
         """Test : test_012_config_has_key_test_unitaries_url"""
-        self.assertRegexpMatches(
-            self.test_config.get("TEST_UNITARIES")["url"],
-            self.regexs[0], self.msgs[13]
+        self.assertRegex(
+            self.test_config['tests']['unitaries']['url'],
+            self.REGEX_URL,
+            self.ERR_KEY_REGEX.format('tests.unitaries.url', self.REGEX_URL)
         )
 
-    def test_013_config_has_key_test_functionals_url_login(self): # TODO: edit test
+    def test_012_key_url_login(self):
         """Test : test_013_config_has_key_test_functionals_url_login"""
-        self.assertRegexpMatches(
-            self.test_config.get("TEST_FUNCTIONALS")["url_login"],
-            self.regexs[0], self.msgs[14]
+        self.assertRegex(
+            self.test_config['tests']['functionals']['url_login'],
+            self.REGEX_URL,
+            self.ERR_KEY_REGEX.format('tests.functionals.url_login', self.REGEX_URL)
         )
 
-    def test_014_config_has_key_test_functionals_url_logout(self): # TODO: edit test
+    def test_013_key_url_logout(self):
         """Test : test_014_config_has_key_test_functionals_url_logout"""
-        self.assertRegexpMatches(
-            self.test_config.get("TEST_FUNCTIONALS")["url_logout"],
-            self.regexs[0], self.msgs[15]
+        self.assertRegex(
+            self.test_config['tests']['functionals']['url_logout'],
+            self.REGEX_URL,
+            self.ERR_KEY_REGEX.format('tests.functionals.url_logout', self.REGEX_URL)
         )
 
-    def test_015_config_has_key_test_functionals_url_logged_ok(self): # TODO: edit test
+    def test_014_key_url_logged_ok(self):
         """Test : test_015_config_has_key_test_functionals_url_logged_ok"""
-        self.assertRegexpMatches(
-            self.test_config.get("TEST_FUNCTIONALS")["url_logged_ok"],
-            self.regexs[0], self.msgs[16]
+        self.assertRegex(
+            self.test_config['tests']['functionals']['url_logged_ok'],
+            self.REGEX_URL,
+            self.ERR_KEY_REGEX.format('tests.functionals.url_logged_ok', self.REGEX_URL)
         )
 
-    def test_016_config_has_key_test_functionals_url_logged_ko(self): # TODO: edit test
+    def test_015_key_url_logged_ko(self):
         """Test : test_016_config_has_key_test_functionals_url_logged_ko"""
-        self.assertRegexpMatches(
-            self.test_config.get("TEST_FUNCTIONALS")["url_logged_ko"],
-            self.regexs[0], self.msgs[17]
+        self.assertRegex(
+            self.test_config['tests']['functionals']['url_logged_ko'],
+            self.REGEX_URL,
+            self.ERR_KEY_REGEX.format('tests.functionals.url_logged_ko', self.REGEX_URL)
         )
 
-    def test_017_config_has_key_test_functionals_selectors_login(self): # TODO: edit test
+    def test_016_key_selectors_login(self):
         """Test : test_017_config_has_key_test_functionals_selectors_login"""
-        values = ast.literal_eval(
-            self.test_config.get("TEST_FUNCTIONALS")["selectors_login"]
-        )
-        self.assertEqual(len(values), 3, self.msgs[18])
+        values = self.test_config['tests']['functionals']['selectors_login']
+        self.assertEqual(len(values), 3, self.ERR_KEY_EMPTY.format(
+            'tests.functionals.selectors_login', values))
         for value in values:
-            self.assertNotEqual(value, "", self.msgs[18])
+            self.assertNotEqual(value, "", self.ERR_KEY_EMPTY.format(
+                'tests.functionals.selectors_login', value))
 
-    def test_018_config_has_key_test_functionals_creed_user(self): # TODO: edit test
+    def test_017_key_creed_user(self):
         """Test : test_018_config_has_key_test_functionals_creed_user"""
-        value = self.test_config.get("TEST_FUNCTIONALS")["creed_user"]
-        self.assertNotEqual(value, "", self.msgs[19])
+        value = self.test_config['tests']['functionals']['creed_user']
+        self.assertNotEqual(value, "", self.ERR_KEY_EMPTY.format(
+            'tests.functionals.creed_user', value))
 
-    def test_019_config_has_key_test_functionals_creed_pass(self): # TODO: edit test
+    def test_018_key_creed_pass(self):
         """Test : test_019_config_has_key_test_functionals_creed_pass"""
-        value = self.test_config.get("TEST_FUNCTIONALS")["creed_pass"]
-        self.assertNotEqual(value, "", self.msgs[20])
+        value = self.test_config['tests']['functionals']['creed_pass']
+        self.assertNotEqual(value, "", self.ERR_KEY_EMPTY.format(
+            'tests.functionals.creed_pass', value))
 
-    def test_020_config_has_key_build_skip_travis_tests(self): # TODO: edit test
+    def test_019_key_skip_travis_tests(self):
         """Test : test_020_config_has_key_build_skip_travis_tests"""
-        value = self.test_config.get("BUILD")["skip_travis_tests"]
-        self.assertNotEqual(value, None, self.msgs[21])
-        self.assertNotEqual(value, "", self.msgs[21])
+        value = self.test_config['build']['travis']['skip_tests']
+        msg = self.ERR_KEY_INVALID_VALUE.format('build.travis.skip_tests', value)
+        self.assertIn(value, (True, False), msg)
