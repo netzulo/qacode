f6f375a4d3552479692f478382d3d34bbc916bac
crypto netzulo <crypto.netzulo@gmail.com>
2017-10-04 21:27:57 +0200

    [qacode]pylint fixes for PageBase+test

--- qacode/core/webs/pages/PageBase.py
+++ qacode/core/webs/pages/PageBase.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""TODO"""
 
 
 from qacode.core.exceptions.PageException import PageException
@@ -16,8 +17,9 @@ class PageBase(object):
     selectors = None
     go_url = None
     elements = []
+    locator = None
 
-    def __init__(self, bot, url, by=By.CSS_SELECTOR, selectors=[],
+    def __init__(self, bot, url, by=By.CSS_SELECTOR, selectors=None,
                  go_url=True):
         """
         required:
@@ -32,43 +34,47 @@ class PageBase(object):
         if bot is None:
             raise PageException("param bot is None")
         self.bot = bot
-        if url is None or len(url) <= 0:
-            raise PageException("param url is None or len <= 0")
+        if url is None:
+            raise PageException(
+                message='Param url can\'t be None or empty')
         self.url = url
         if by is None:
-            self.by = By.CSS_SELECTOR
-        else:
-            self.by = by
-        if selectors is None:
-            raise PageException("param selectors is None")
+            raise PageException(message='Param by can\'t be None')
+        self.locator = by
         self.selectors = selectors
         if go_url is None:
-            raise PageException("param go_url is None")
+            raise PageException(message='Param go_url can\'t be None')
         self.go_url = go_url
-        if go_url:
+        if self.go_url:
             self.go_page_url()
-        if len(selectors) > 0:
+
+        if self.selectors is not None:
             self.elements = self.get_elements()
 
-    def get_elements(self, selectors=[]):
+    def get_elements(self, selectors=None):
+        """
+        Search elements on Bot instance, choose selectors
+          from instance or by param
+        """
         searchs = None
         elements = []
-        if len(selectors) <= 0:
+        if selectors is None:
             searchs = self.selectors
         else:
             searchs = selectors
         for selector in searchs:
-            message_template = "Searching element: by={} with selector={}"
-            self.bot.log.debug(message_template.format(self.by, selector))
-            element = self.bot.navigation.find_element(selector, self.by)
+            message_template = "Searching element: with selector={} by={}"
+            self.bot.log.debug(message_template.format(selector, self.locator))
+            element = self.bot.navigation.find_element(selector, self.locator)
             if element is None:
-                self.bot.log.error(message_template.format(self.by, selector))
+                self.bot.log.error(message_template.format(selector, self.locator))
             else:
                 self.bot.log.debug("Element Found, adding to return method")
                 elements.append(element)
         return elements
 
     def go_page_url(self, url=None, wait_for_load=0):
+        """Go to url, choose url from instance or by params"""
         if url is None:
             self.bot.navigation.get_url(self.url, wait_for_load=wait_for_load)
         else:
