56433c011fe025189912e24c997acced69aa29d8
crypto netzulo <crypto.netzulo@gmail.com>
2017-10-07 16:09:58 +0200

    [qacode]pylint+tests ControlBase

--- qacode/core/exceptions/ControlException.py
+++ qacode/core/exceptions/ControlException.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+"""Package qacode.core.exceptions"""
 
 
-class ControlException(object):
+from qacode.core.exceptions.CoreException import CoreException
+
+
+class ControlException(CoreException):
     """ControlBase and inherit classes can use this exception"""
 
     def __init__(
             self,
+            err=None,
             message="Message Exception not defined for ControlException class",
             log=None
     ):
-        super(ControlException, self).__init__(message, log)
+        super(ControlException, self).__init__(err=err, message=message, log=log)
