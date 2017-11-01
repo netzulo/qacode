c1c30063f678f8b1d00cb88021cbc081ec1e70a6
crypto netzulo <crypto.netzulo@gmail.com>
2017-11-01 20:49:48 +0100

    [qacode] update to v0.3.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(file_name=None, is_encoding=True, ignore_raises=False):
 
 setup(
     name='qacode',
-    version='0.3.1',
+    version='0.3.2',
     license=read("LICENSE", is_encoding=False, ignore_raises=True),
     packages=find_packages(exclude=['tests']),
     description='Main automation lib',
@@ -43,7 +43,7 @@ setup(
     author='Netzulo Open Source',
     author_email='netzuleando@gmail.com',
     url='https://github.com/netzulo/qacode',
-    download_url='https://github.com/netzulo/qacode/tarball/v0.3.1',
+    download_url='https://github.com/netzulo/qacode/tarball/v0.3.2',
     keywords=['testing', 'logging', 'functional', 'selenium', 'test'],
     install_requires=[
         'appdirs',
