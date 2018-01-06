54682de0e55f1e67a9613d1d278fd6d6de30773b
crypto netzulo <crypto.netzulo@gmail.com>
2018-01-06 18:41:43 +0100

    [qacode] fix new text issue #94

--- tests/000_unitaries/suite_001_logger.py
+++ tests/000_unitaries/suite_001_logger.py
@@ -41,5 +41,6 @@ class TestLoggerManager(TestInfoBase):
 
     def test_006_logger_loglevelbyconfig(self):
         """Test: test_005_logger_critical"""
-        self.log = LoggerManager(log_level=SETTINGS['bot']['log_level'])
+        self.logger_mamager_test = LoggerManager(log_level=SETTINGS['bot']['log_level'])
+        self.log = self.logger_mamager_test.logger
         self.log.warning("Unitary test, checking level type by JSON key")
