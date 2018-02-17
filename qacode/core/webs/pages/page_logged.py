a4eea06f9642d51ef0eae956fcb1d2b8c26ec6aa
crypto netzulo <crypto.netzulo@gmail.com>
2018-02-17 12:53:40 +0100

    [qacode] finishing issue #34

--- qacode/core/webs/pages/page_logged.py
+++ qacode/core/webs/pages/page_logged.py
@@ -16,7 +16,6 @@ class PageLogged(PageBase):
     """
 
     url_logout = None
-    btn_logout = None
 
     def __init__(self, bot, url, url_logout, selectors=None, locator=By.CSS_SELECTOR,
                  go_url=True, maximize=False):
@@ -28,30 +27,22 @@ class PageLogged(PageBase):
         Optionals:
             selectors: a list of selectors ready to search elements
             locator: change default locator selector 'CSS_SELECTOR'
-            go_url_maximize: change default page load,
-             open browser maximized
+            go_url: change default page load
+            maximize: open browser maximized
         """
         super(PageLogged, self).__init__(
             bot, url, selectors, locator=locator,
             go_url=go_url, maximize=maximize)
-        if selectors is None:
-            raise PageException(message='Param selectors can\'t be None')
-        if len(selectors) != 1:
-            raise PageException(
-                message='Can\'t instance PageLogin elements '
-                'need at least 1 selector'
-            )
         if url_logout is None:
             raise PageException(message='Param url_logout can\'t be None')
         self.url_logout = url_logout
-        self.btn_logout = self.elements[0]
 
     def logout(self):
-        """Allows to do logout on a logged web page using button click"""
-        self.btn_logout.click()
+        """Allows to do logout on a logged web page using an url request"""
+        self.go_url()
         is_url_logout = self.is_url(url=self.url_logout)
         if not is_url_logout:
-            raise PageException('logout failed after click')
+            raise PageException('logout failed after going url_logout')
         return True
 
     def is_logged(self, page_login):
