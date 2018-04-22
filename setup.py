d60b9305abffd6b8b6ab8310f08b746b62ca65c3
netzulo <netzuleando@gmail.com>
2018-04-22 22:45:02 +0200

    [qacode] update to v0.4.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from qacode.core.utils import read_file
 from qacode.core.utils import path_format
 
 
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 CURR_PATH = "{}{}".format(path.abspath(path.dirname(__file__)), '/')
 
 
