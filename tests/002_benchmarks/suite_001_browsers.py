b932864c5332594c1e70ac006663a2b2b17979f6
netzulo <netzuleando@gmail.com>
2018-05-13 19:43:51 +0200

    [qacode] skip added

--- tests/002_benchmarks/suite_001_browsers.py
+++ tests/002_benchmarks/suite_001_browsers.py
@@ -10,8 +10,8 @@ from qacode.core.utils import settings
 
 
 SETTINGS = settings()
-SKIP_LOCALS = SETTINGS['tests']['skip']['benchmarks']
-SKIP_LOCALS_MSG = 'benchmarks DISABLED by config file'
+SKIP = SETTINGS['tests']['skip']['benchmarks']
+SKIP_MSG = 'benchmarks DISABLED by config file'
 # TODO: must be setteable from config JSON
 WAIT_TO_CLOSE = int(3)
 LOGGER_MANAGER = LoggerManager(log_level=SETTINGS['bot']['log_level'])
@@ -23,7 +23,7 @@ class TestBotBase(TestInfoBase):
     def setup_method(self, test_method):
         """TODO: doc method"""
         super(TestBotBase, self).setup_method(
-            test_method, config=settings())
+            test_method, config=settings(), skip_force=SKIP)
 
     def bot_benchmark(self, browser_name, driver_mode):
         """Allow to open and close any browser_name with
@@ -76,8 +76,6 @@ class TestBotBase(TestInfoBase):
     @pytest.mark.parametrize("driver_mode", ["local", "remote"])
     def test_benchmark_browsers(self, benchmark, browser_name, driver_mode):
         """Testcase: test_benchmark_local_chrome"""
-        if SKIP_LOCALS:
-            pytest.skip(SKIP_LOCALS_MSG)
         cfg_local = {'browser_name': browser_name, 'driver_mode': driver_mode}
         benchmark.pedantic(
             self.bot_benchmark, kwargs=cfg_local, iterations=1, rounds=1)
