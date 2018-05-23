b8f2fb92868c10b6e8ef9c2f4c261c26e8b914ae
josue.garcia <josue.garcia@gigas.com>
2018-05-23 10:10:49 +0200

    [qacode] Update to v0.5.1
    + Added headless mode for CHROME
    + Added headless mode for FIREFOX
    + Added headless mode for OPERA
    
    - Removed PHANTOMJS driver, obsolete by selenium

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 from sys import version_info
 
 
-VERSION = "0.5.0"
+VERSION = "0.5.1"
 CURR_PATH = "{}{}".format(path.abspath(path.dirname(__file__)), '/')
 
 INSTALL_REQUIRES = [
