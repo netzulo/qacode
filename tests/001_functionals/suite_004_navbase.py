fc3f51edb15524b9413743a245d5d44ea0f4a458
netzulo <netzuleando@gmail.com>
2018-06-10 20:55:50 +0200

    [qacode] fix for issue #150 + flake8 errors

--- tests/001_functionals/suite_004_navbase.py
+++ tests/001_functionals/suite_004_navbase.py
@@ -34,71 +34,49 @@ class TestNavBase(TestInfoBotUnique):
 
     @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
     def test_navbase_instance(self):
-        """Testcase: test_000_navbase_instance"""
+        """Testcase: test_navbase_instance"""
         self.assert_is_instance(self.bot.navigation, NavBase)
 
     @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
     def test_gourl_without_waits(self):
-        """Testcase: test_001_gourl_without_waitsparam"""
+        """Testcase: test_gourl_without_waits"""
         self.bot.navigation.get_url(self.page_home.get('url'))
 
     @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
     def test_gourl_with_waitsparam(self):
-        """Testcase: test_002_gourl_with_waitsparam"""
+        """Testcase: test_gourl_with_waitsparam"""
         self.bot.navigation.get_url(
             self.page_home.get('url'), wait_for_load=0)
 
     @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
     def test_getmaximizewindow(self):
-        """Testcase: test_003_getmaximizewindow"""
+        """Testcase: test_getmaximizewindow"""
         self.bot.navigation.get_maximize_window()
 
     @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
     def test_getcapabilities(self):
-        """Testcase: test_004_getcapabilities"""
+        """Testcase: test_getcapabilities"""
         caps = self.bot.navigation.get_capabilities()
         self.assert_is_instance(caps, dict)
         self.assert_is_instance(caps['chrome'], dict)
         self.assert_equals(caps['browserName'], 'chrome')
 
     @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
-    def test_getlog(self):
-        """Testcase: test_005_getlog"""
+    def test_getlog_default(self):
+        """Testcase: test_getlog_default"""
         self.bot.navigation.get_url(self.page_home.get('url'))
         log_data = self.bot.navigation.get_log()
         self.assert_not_none(log_data)
         self.log.debug("selenium logs, browser={}".format(log_data))
 
     @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
-    def test_getlog_browser(self):
-        """Testcase: test_006_getlog_browser"""
+    @pytest.mark.parametrize(
+        "log_name", ['browser', 'driver', 'client', 'server'])
+    def test_getlog_lognames(self, log_name):
+        """Testcase: test_getlog_logname"""
         self.bot.navigation.get_url(self.page_home.get('url'))
-        log_data = self.bot.navigation.get_log(log_name='browser')
+        log_data = self.bot.navigation.get_log(log_name=log_name)
         self.assert_not_none(log_data)
-        self.log.debug("selenium logs, browser={}".format(log_data))
-
-    @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
-    def test_getlog_driver(self):
-        """Testcase: test_007_getlog_driver"""
-        self.bot.navigation.get_url(self.page_home.get('url'))
-        log_data = self.bot.navigation.get_log(log_name='driver')
-        self.assert_not_none(log_data)
-        self.log.debug("selenium logs, driver={}".format(log_data))
-
-    @pytest.mark.skip('Issue opened on github selenium, waiting...')
-    def test_getlog_client(self):
-        """Testcase: test_008_getlog_client
-        https://github.com/SeleniumHQ/selenium/issues/5410
-        """
-        self.bot.navigation.get_url(self.page_home.get('url'))
-        log_data = self.bot.navigation.get_log(log_name='client')
-        self.assert_not_none(log_data)
-        self.log.debug("selenium logs, client={}".format(log_data))
-
-    @pytest.mark.skipIf(SKIP_REMOTES, SKIP_REMOTES_MSG)
-    def test_getlog_server(self):
-        """Testcase: test_008_getlog_client"""
-        self.bot.navigation.get_url(self.page_home.get('url'))
-        log_data = self.bot.navigation.get_log(log_name='server')
-        self.assert_not_none(log_data)
-        self.log.debug("selenium logs, server={}".format(log_data))
+        msg = "selenium logs, log_name={}, log_data={}".format(
+            log_name, log_data)
+        self.log.debug(msg)
