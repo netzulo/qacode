65e72f48bb43abaf500fd4b5d1ed63148be6d79c
netzulo <netzuleando@gmail.com>
2020-01-28 21:59:59 +0100

    [qacode] removed dependencies
    + update logger , now need refactor the rest

--- qacode/core/loggers/logger_manager.py
+++ qacode/core/loggers/logger_manager.py
@@ -4,6 +4,7 @@
 
 from logging import (
     FileHandler, Formatter, INFO, StreamHandler, getLogger)
+from qacode.utils import path_format
 
 
 class Log(object):
@@ -11,7 +12,9 @@ class Log(object):
 
     # ATTRIBUTES
     _name = None
+    _name_file = None
     _path = None
+    _path_join = None
     _level = None
     _formatter = None
     _logger = None
@@ -19,7 +22,10 @@ class Log(object):
     def __init__(self, *args, **kwargs):
         """TODO: doc method"""
         self._name = kwargs.get('log_name') or "qacode"
-        self._path = kwargs.get('log_path') or './logs/{}.log'.format(self._name)
+        self._name_file = "{}.log".format(self._name)
+        self._path = kwargs.get('log_path') or './logs/'
+        self._path_join = path_format(
+            file_path=self._path, file_name=self._name_file)
         self._level = kwargs.get('log_level') or INFO
 
         self._formatter = Formatter(
@@ -42,7 +48,7 @@ class Log(object):
 
     def __handlers__(self):
         """TODO: doc method"""
-        handlers = [StreamHandler(), FileHandler(self._path)]
+        handlers = [StreamHandler(), FileHandler(self._path_join)]
         for handler in handlers:
             handler.setFormatter(self._formatter)
             handler.setLevel(self._level)
