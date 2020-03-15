d73efbeca2891c5fbc7afbe23a88ca48158868e7
netzulo <netzuleando@gmail.com>
2020-03-15 21:19:53 +0100

    [qacode] fixes for codeclimate

--- qacode/core/browsers/modules/waits.py
+++ qacode/core/browsers/modules/waits.py
@@ -16,65 +16,54 @@ class ModuleWaits(Module):
         self._driver = driver
         self._driver_wait = driver_wait
 
+    def __driver_wait__(self, timeout=None):
+        """TODO: doc method"""
+        driver_wait = self._driver_wait
+        if timeout:
+            driver_wait = WebDriverWait(self._driver, timeout)
+        return driver_wait
+
     def find_wait(self, selector, locator=By.CSS_SELECTOR, timeout=None):
         """Search element using WebDriverWait class
             and ElementConditions presence_of_element_located
         """
-        driver_wait = self._driver_wait
         self.__check_not_none__("locator", locator)
-        if timeout:
-            driver_wait = WebDriverWait(self._driver, timeout)
         # Not found: NoSuchElementException, StaleElementReferenceException
         expectation = EC.presence_of_element_located((locator, selector))
-        return driver_wait.until(expectation)
+        return self.__driver_wait__(timeout=timeout).until(expectation)
 
     def finds_wait(self, selector, locator=By.CSS_SELECTOR, timeout=None):
         """Search elements using WebDriverWait class
             and ElementConditions presence_of_all_elements_located
         """
-        driver_wait = self._driver_wait
         self.__check_not_none__("locator", locator)
-        if timeout:
-            driver_wait = WebDriverWait(self._driver, timeout)
         # Not found: NoSuchElementException, StaleElementReferenceException
         expectation = EC.presence_of_all_elements_located((locator, selector))
-        return driver_wait.until(expectation)
+        return self.__driver_wait__(timeout=timeout).until(expectation)
 
     def ele_invisible(self, selector, locator=By.CSS_SELECTOR, timeout=None):
         """Wait for invisible element (display:none), returns element"""
-        driver_wait = self._driver_wait
         self.__check_not_none__("selector", selector)
-        if timeout:
-            driver_wait = WebDriverWait(self._driver, timeout)
         expectation = EC.invisibility_of_element_located((locator, selector))
-        return driver_wait.until(expectation)
+        return self.__driver_wait__(timeout=timeout).until(expectation)
 
     def ele_visible(self, element, timeout=None):
         """Wait for visible condition element, returns self"""
-        driver_wait = self._driver_wait
         self.__check_not_none__("element", element)
-        if timeout:
-            driver_wait = WebDriverWait(self._driver, timeout)
         expectation = EC.visibility_of(element)
-        return driver_wait.until(expectation)
+        return self.__driver_wait__(timeout=timeout).until(expectation)
 
     def ele_text(self, selector, text, locator=By.CSS_SELECTOR, timeout=None):
         """Wait if the given text is present in the specified element"""
-        driver_wait = self._driver_wait
         self.__check_not_none__("selector", selector)
-        if timeout:
-            driver_wait = WebDriverWait(self._driver, timeout)
         expectation = EC.text_to_be_present_in_element(
             (locator, selector), text)
-        return driver_wait.until(expectation)
+        return self.__driver_wait__(timeout=timeout).until(expectation)
 
     def ele_value(self, selector, value, locator=By.CSS_SELECTOR,
                   timeout=None):
         """Wait if the given value is present in the specified element"""
-        driver_wait = self._driver_wait
         self.__check_not_none__("selector", selector)
-        if timeout:
-            driver_wait = WebDriverWait(self._driver, timeout)
         expectation = EC.text_to_be_present_in_element_value(
             (locator, selector), value)
-        return driver_wait.until(expectation)
+        return self.__driver_wait__(timeout=timeout).until(expectation)
